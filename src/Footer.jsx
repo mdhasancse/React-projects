5cb5e44a17019b61f0ffa29fc20979a45def626c
Hasan Md <hasan.md@xencia.com>
2024-01-18 15:23:44 +0530

    Header list and footer develped

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return <div className="footer">
+        <footer>
+            <p>Author: Hege Refsnes oioijoi <br></br>
+                <a href="mailto:hege@example.com">hege@example.com</a></p>
+        </footer>
+    </div>
+}
+export default Footer
---
+++ src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List(){
+    return <div className="list">
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+            <li>Five</li>
+            <li>Six</li>
+        </ul>
+    </div>
+}
+export default List
